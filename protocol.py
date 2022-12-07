75487e36e82f9b177d47a71484197e69ff529c9b
a1batross <a1ba.omarov@gmail.com>
2022-12-07 21:21:49 +0100

    protocol: declare header sequences as bytes

--- protocol.py
+++ protocol.py
@@ -1,15 +1,14 @@
 class MasterProtocol:
-	
 	# Client To Master
-	clientQuery = '1'
-	
+	clientQuery = b'1'
+
 	# Server To Master
-	challengeRequest = 'q'
-	addServer = '0'
-	removeServer = 'b'
-	
+	challengeRequest = b'q\xFF'
+	addServer = b'0\n'
+	removeServer = b'\x62\x0A'
+
 	# Master To Client
 	queryPacketHeader = b'\xff\xff\xff\xff\x66\x0a'
-	
+
 	# Master To Server
 	challengePacketHeader = b'\xff\xff\xff\xff\x73\x0a'
