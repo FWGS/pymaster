273991e58f66749f23ab7dc353504f2df3b12a09
ghost <localhost>
2023-12-24 06:25:58 +0200

    fix undefined variable

--- pymaster.py
+++ pymaster.py
@@ -130,7 +130,7 @@ class PyMaster:
 					self.fakeInfoForOldVersions(gamedir, addr)
 					return
 				else:
-					log('Client Query: %s:%d, invalid infostring=%s' % (addr[0], addr[1], rawFilter))
+					log('Client Query: %s:%d, invalid infostring=%s' % (addr[0], addr[1], data))
 			except IndexError:
 				pass
 
